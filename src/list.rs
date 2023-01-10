0b70cf2ad54ecb2a73875d9a1a5b7b3e24747ca6
Tristan Fox <tristanfox009@outlook.com>
2023-01-11 09:35:34 +1100

    feat: Add '--tree' option to list command

--- src/list.rs
+++ src/list.rs
@@ -4,19 +4,29 @@ use miette::Result;
 use crate::{repository::Repository, strip_namespace};
 
 #[derive(Args)]
-pub struct ListOptions {}
+pub struct ListOptions {
+    #[clap(short, long)]
+    tree: bool,
+}
 
-pub fn list(repository: &Repository, _options: ListOptions) -> Result<()> {
+pub fn list(repository: &Repository, options: ListOptions) -> Result<()> {
     for (name, path) in repository.namespaces()? {
-        let dir = repository.dir.join(name);
+        let dir = repository.dir.join(&name);
         let entries = dir.read_dir().expect("Failed to read directory");
+        if options.tree {
+            println!("{}: {}", name, path.display());
+        }
         for entry in entries {
             let entry = entry.expect("Invalid entry");
             // Don't include namespace files
             if entry.path().file_name().unwrap() != "namespace.fig" {
                 let display_path =
                     strip_namespace(&dir, entry.path()).expect("Failed to strip namespace");
-                println!("\t{}", path.join(display_path).display());
+                println!(
+                    "{}{}",
+                    if options.tree { "\t" } else { "" },
+                    path.join(display_path).display()
+                );
             }
         }
     }
