fb766ab1e2018b0a81ce69cb216a9b3f6ea335da
Tristan Fox <tristanfox009@outlook.com>
2023-01-29 19:08:08 +1100

    fix: list command now recurses into subdirectories

--- src/list.rs
+++ src/list.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use clap::Args;
 use miette::Result;
 
@@ -12,24 +14,31 @@ pub struct ListOptions {
 pub fn list(repository: &Repository, options: ListOptions) -> Result<()> {
     for (name, path) in repository.namespaces()? {
         let dir = repository.dir.join(&name);
-        let entries = dir.read_dir().expect("Failed to read directory");
         if options.tree {
             println!("{}: {}", name, path.display());
         }
-        for entry in entries {
-            let entry = entry.expect("Invalid entry");
-            // Don't include namespace files
-            if entry.path().file_name().unwrap() != "namespace.fig" {
-                let display_path =
-                    strip_namespace(&dir, entry.path()).expect("Failed to strip namespace");
-                println!(
-                    "{}{}",
-                    if options.tree { "\t" } else { "" },
-                    path.join(display_path).display()
-                );
-            }
-        }
+        recurse_dir(&options, &dir);
     }
 
     Ok(())
+}
+
+fn recurse_dir(options: &ListOptions, path: impl AsRef<Path>) {
+    let path = path.as_ref();
+    let entries = path.read_dir().expect("Failed to read directory");
+    for entry in entries {
+        let entry = entry.expect("Invalid entry");
+        // Don't include namespace files
+        if entry.path().file_name().unwrap() != "namespace.fig" && entry.path().is_file() {
+            let display_path =
+                strip_namespace(&path, entry.path()).expect("Failed to strip namespace");
+            println!(
+                "{}{}",
+                if options.tree { "\t" } else { "" },
+                path.join(display_path).display()
+            );
+        } else if entry.path().is_dir() {
+            recurse_dir(options, entry.path());
+        }
+    }
 }
