0417af5c1c3cc1c918d1977672836665c3f496a1
Tristan Fox <tristanfox009@outlook.com>
2023-04-02 12:30:11 +1000

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = crane.lib.${system}.buildPackage {
+        src = ./.;
+      };
+    });
+}
