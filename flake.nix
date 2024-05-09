0af8f868794f615bab8a1ff931d443dd1ca45efb
Tristan Fox <foxtristan@proton.me>
2024-05-10 09:58:45 +1000

    Add fig as package to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
   inputs = {
-    rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
   outputs =
@@ -10,6 +19,7 @@
       nixpkgs,
       utils,
       rust-overlay,
+      naersk,
       ...
     }:
     utils.lib.eachDefaultSystem (
@@ -17,8 +27,19 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+        rust = (pkgs.rust-bin.stable.latest.default);
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
       in
-      {
+      rec {
+        packages.fig = naersk-lib.buildPackage {
+          src = ./.;
+          pname = "fig";
+          buildInputs = with pkgs; [ pkg-config openssl ];
+        };
+        defaultPackage = packages.fig;
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             (rust-bin.stable.latest.default.override {
